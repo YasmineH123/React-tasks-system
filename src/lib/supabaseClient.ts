5ed94e0d1d2a1c3266136248fc2d72f09741df84
YasmineH123 <elhaririym@students.rhu.edu.lb>
2026-03-26 16:43:23 +0200

    --

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
  
-const supabaseUrl =  import.meta.env.VITE_SUPABASE_URL;
-const supaAnone = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://moliabvuugraoecrirjf.supabase.co";
+const supaAnone = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vbGlhYnZ1dWdyYW9lY3JpcmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4ODkzODYsImV4cCI6MjA4OTQ2NTM4Nn0.uiSZBlgWDK3bXPZ7klywW5WLKjNbAniMvrRRf6035YI";
  
-console.log(import.meta.env);
 export const supabase = createClient(supabaseUrl, supaAnone);
